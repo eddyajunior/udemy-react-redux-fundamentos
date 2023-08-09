4f02ebedb281a028aaabbbd40f5cdc0014946d0a
Edson Amaral <eddyajunior@gmail.com>
2023-08-09 13:49:54 -0300

    Video #14 - Carregando CSS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom";
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let tag = "Eu sou uma tag!";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <div>
+    <strong>Olá React</strong>
+    <div>{tag}</div>
+  </div>, 
+  document.getElementById('root')
+  );
